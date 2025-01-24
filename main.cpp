5ac8d165e490b2158e115009bc77d88c778298e5
Zelephir1 <mael.caetano@ynov.com>
2025-01-24 12:56:22 +0100

    ~~~~

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ int main()
 	instruments.push_back(theremine);
 	instruments.push_back(instrument);
 
-	cout << " ~~~~ BIENVUENUE CHEZ MUSICALOU ~~~" << endl << endl << "Voici les instruments que nous avons en stock : " << endl << endl;
+	cout << "~~~ BIENVUENUE CHEZ MUSICALOU ~~~" << endl << endl << "Voici les instruments que nous avons en stock : " << endl << endl;
 
 	int choixInstrument = 0;
 
